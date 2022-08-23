3843fc08cb37fc819364d6d100132bbfa760edae
Apervush <apervushin@perfobur.com>
2022-08-23 09:00:58 +0500

    Start project

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+    private static final String URL = "jdbc:postgresql://localhost/postgres?user=postgres&password=Timon1";
+    private static Connection con;
+    public static void main(String[] args) {
+        try {
+            con = DriverManager.getConnection(URL);
+            con.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+}
